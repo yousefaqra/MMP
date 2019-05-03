7f91c0235dd4ab931819fcd4d57307eb2d076f5b
Yousef Alaqra <aqray30@gmail.com>
2019-05-03 14:31:22 +0300

    Setup the solution

--- Web.Api/Startup.cs
+++ Web.Api/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace MMP
+namespace Web.Api
 {
     public class Startup
     {
